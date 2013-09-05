4a5ebfa6b4ed7cfd8db4b56016916ffca00b43aa
sabroso <zarnatron@gmail.com>
2013-09-05 13:49:10 -0500

    Comparte

---
+++ comparte.php
@@ -0,0 +1,22 @@
+<!-- estas metas deben de ir entre las etiquetas  <head></head>-->
+<meta property="og:title" content=""/>
+<meta property="og:description" content="" />
+<meta property="og:image" content="http://dominio/img/imagen.jpg"/>
+<meta property="og:url" content="http://dominio.com"/>
+<!-- Y este es es el codigo que comparte la pagina ponlo donde mas te convenga -->
+<a href="" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436,left=300,top=150'); return false;">
+<img src="img/imagen.png" alt="facebook">
+</a>
+<!-- Este es el codigo de Google ++ ponlo donde mas te convenga -->
+<a href="https://plus.google.com/share?url={http://dominio.com}" onclick="javascript:window.open(this.href,'',
+'width=626,height=436,left=300,top=150');
+return false;">
+<img src="img/google.png" alt="Share on Google+"/>  
+ </a>
+<!-- Este es el codigo para Twitter ponlo donde mas te convenga-->
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://dominio.com" data-text="Charolastra" data-lang="es" data-size="large">
+Twittear
+</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+</script>
